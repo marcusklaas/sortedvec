7cc101ca8334a966f026baff61337bf4845a6900
Marcus Klaas <mail@marcusklaas.nl>
2019-02-22 17:55:58 +0100

    Get to something quick and functional

--- benches/lib.rs
+++ benches/lib.rs
@@ -96,14 +96,39 @@ mod int_bench {
 
     use faster::*;
 
-    const SHARED_LEN: usize = 100;
+    const SHARED_LEN: usize = 800;
+
+    fn usize_common_prefix_len(a: &[u8], b: &[u8]) -> usize {
+        let usize_width = std::mem::size_of::<usize>();
+        let shared_len = std::cmp::min(a.len(), b.len());
+        let usize_len = shared_len / usize_width;
+
+        let a_usize: &[usize] = unsafe { std::mem::transmute(a) };
+        let b_usize: &[usize] = unsafe { std::mem::transmute(b) };
+
+        let prefix_len = a_usize[..usize_len].iter().zip(b_usize[..usize_len].iter()).take_while(|(a, b)| a == b).count() * usize_width;
+        prefix_len + a[prefix_len..shared_len].iter().zip(b[prefix_len..shared_len].iter()).take_while(|(a, b)| a == b).count()
+    }
+
+    #[bench]
+    fn common_prefix_usize(b: &mut test::Bencher) {
+        let a: Vec<u8> = ::std::iter::repeat(127u8).take(SHARED_LEN).chain(Some(5u8)).collect();
+        let c: Vec<u8> = ::std::iter::repeat(127u8).take(SHARED_LEN).chain(Some(4u8)).collect();
+        
+        b.iter(|| assert_eq!(SHARED_LEN, usize_common_prefix_len(&a, &c)));
+    }
+
+    fn common_prefix_len(a: &[u8], b: &[u8]) -> usize {
+        let shared_len = std::cmp::min(a.len(), b.len());
+        a[..shared_len].iter().zip(b[..shared_len].iter()).take_while(|(a, b)| a == b).count()
+    }
 
     #[bench]
     fn common_prefix(b: &mut test::Bencher) {
         let a: Vec<u8> = ::std::iter::repeat(127u8).take(SHARED_LEN).chain(Some(5u8)).collect();
         let c: Vec<u8> = ::std::iter::repeat(127u8).take(SHARED_LEN).chain(Some(4u8)).collect();
-
-        b.iter(|| assert_eq!(SHARED_LEN, a.iter().zip(c.iter()).take_while(|(f, e)| e == f).count()));
+        
+        b.iter(|| assert_eq!(SHARED_LEN, common_prefix_len(&a, &c)));
     }
 
     fn simd_common_prefix_len(a: &[u8], b: &[u8]) -> usize {
@@ -112,7 +137,7 @@ mod int_bench {
         let width = iter.width();
 
         let mut prefix_len = iter.simd_map(|(a, b)| a^b).take_while(|&x| x == Default::default()).count() * width;
-        prefix_len += a[prefix_len..shared_len].iter().zip(a[prefix_len..shared_len].iter()).take_while(|(a, b)| a == b).count();
+        prefix_len += a[prefix_len..shared_len].iter().zip(b[prefix_len..shared_len].iter()).take_while(|(a, b)| a == b).count();
         prefix_len
     }
 
@@ -122,5 +147,44 @@ mod int_bench {
         let c: Vec<u8> = ::std::iter::repeat(127u8).take(SHARED_LEN).chain(Some(4u8)).collect();
 
         b.iter(|| assert_eq!(SHARED_LEN, simd_common_prefix_len(&a, &c)));
+    }
+
+    fn simd_alternative(a: &[u8], b: &[u8]) -> usize {
+        let shared_len = std::cmp::min(a.len(), b.len());
+        let first_iter = a[..shared_len].simd_iter(u8s(0));
+        let width = first_iter.width();
+
+        if width <= shared_len {
+            let prefix_len = if shared_len > width {
+                let combined_iter = (first_iter, b[..shared_len].simd_iter(u8s(0))).zip().simd_map(|(a, b)| a^b);
+                combined_iter.take_while(|&x| x == Default::default()).count() * width
+            } else {
+                0
+            };
+
+            if prefix_len < shared_len {
+                let ix = std::cmp::min(shared_len - width, prefix_len);
+                let compared = unsafe {
+                    let a_loaded = a.load_unchecked(ix);
+                    let b_loaded = b.load_unchecked(ix);
+                    a_loaded.eq(b_loaded)
+                };
+                let bitmask = !compared.bitmask();
+
+                ix + bitmask.trailing_zeros() as usize
+            } else {
+                prefix_len
+            }
+        } else {
+            usize_common_prefix_len(a, b)
+        }
+    }
+
+    #[bench]
+    fn common_prefix_simd_alternative(b: &mut test::Bencher) {
+        let a: Vec<u8> = ::std::iter::repeat(127u8).take(SHARED_LEN).chain(Some(5u8)).collect();
+        let c: Vec<u8> = ::std::iter::repeat(127u8).take(SHARED_LEN).chain(Some(4u8)).collect();
+
+        b.iter(|| assert_eq!(SHARED_LEN, simd_alternative(&a, &c)));
     }
 }
